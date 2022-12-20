47d1f018742b0a8130a9350b15d7bed6cadfc8e6
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-12-19 22:42:43 -0300

    Criando o elemento de navegação

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,24 @@
-import { Container } from "./styles"
+import { Container } from './styles'
+
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import menuImg from '../../assets/menu.svg'
 
 export function Sidebar() {
     return (
-        <Container>Sidebar</Container>
+        <Container>
+            <button type='button'>
+                <img src={menuImg} alt='[Abrir e fechar o menu]' title='Abrir e fechar o menu' />
+            </button>
+            <nav>
+                <ul>
+                    <li>
+                        <a href='#'>
+                            <BurgerIcon />
+                            <span>Hambúrgueres</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </Container>
     )
 }
