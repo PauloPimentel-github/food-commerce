0fba6a3a4e03279564f058976e0754d4945abfbb
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-12-20 22:41:19 -0300

    Logica de expansão do menu

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import { Container } from './styles'
 
+import { useState } from 'react'
+
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
 import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
 import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
@@ -8,9 +10,15 @@ import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 import menuImg from '../../assets/menu.svg'
 
 export function Sidebar() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleToggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
-        <Container>
-            <button type='button'>
+        <Container isMenuOpen={menuOpen}>
+            <button type='button' onClick={handleToggleMenu}>
                 <img src={menuImg} alt='[Abrir e fechar o menu]' title='Abrir e fechar o menu' />
             </button>
             <nav>
