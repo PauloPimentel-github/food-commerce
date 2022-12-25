428968e77c1c4a5e637c34f166381cf4c2a01988
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-12-25 00:19:41 -0300

    Recuperando os dados de Burgers via API

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,32 +1,27 @@
+import { useState, useEffect } from 'react'
+
+import { getBurgers } from '../../../services/api'
+
 import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
 
 export default function Burgers() {
-    const data = [
-        {
-            id: 1,
-            snack: 'burgers',
-            name: 'Extra bacon',
-            description: 'Criado para os amantes de bacon, possui em topdas as suas camadas bacon bem assado a ainda queijo e carne.',
-            price: 23.5,
-            image: 'https://i.imgur.com/upjIUnG.jpg'
-        },
-        {
-            id: 2,
-            snack: 'burgers',
-            name: 'Extra bacon',
-            description: 'Criado para os amantes de bacon, possui em topdas as suas camadas bacon bem assado a ainda queijo e carne.',
-            price: 23.5,
-            image: 'https://i.imgur.com/upjIUnG.jpg'
-        }
-    ]
+    const [burgers, setBurgers] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            const burgerRequest = await getBurgers()
+
+            setBurgers(burgerRequest.data)
+        })()
+    }, [])
 
     return (
         <>
             <Head title='Hambúrgueres' description='Nossos melhores burgers' />
             <SnackTitle>Hambúrgeres</SnackTitle>
-            <Snacks snacks={data}></Snacks>
+            <Snacks snacks={burgers}></Snacks>
         </>
     )
 }
