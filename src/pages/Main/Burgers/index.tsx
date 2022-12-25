239155527bcfdab418b5dcc27aa5a58453183f6c
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-12-24 22:24:48 -0300

    Renderizando Burgers

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,5 +1,6 @@
 import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
+import { Snacks } from '../../../components/Snacks'
 
 export default function Burgers() {
     const data = [
@@ -17,17 +18,15 @@ export default function Burgers() {
             name: 'Extra bacon',
             description: 'Criado para os amantes de bacon, possui em topdas as suas camadas bacon bem assado a ainda queijo e carne.',
             price: 23.5,
-            image: 'https://i.imgur.com/B4J-4AJ.jpg'
+            image: 'https://i.imgur.com/upjIUnG.jpg'
         }
     ]
-
-    console.log('data', data);
-    
 
     return (
         <>
             <Head title='Hambúrgueres' description='Nossos melhores burgers' />
             <SnackTitle>Hambúrgeres</SnackTitle>
+            <Snacks snacks={data}></Snacks>
         </>
     )
 }
